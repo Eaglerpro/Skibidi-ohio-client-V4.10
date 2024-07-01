687833a7542cf1aa2f325cb1073803bc294030c3
Max <Djglitchyt@gmail.com>
2024-07-01 15:14:50 -0400

    Add files via upload

---
+++ vape.bat
@@ -0,0 +1,7 @@
+echo off
+mode 55, 9
+title Starting Vape...
+echo Vape v4.10 cracked by Decencies Reposted by BedFullpro/Eaglerpro
+ping localhost -n 5.5 >nul
+title Vape Client
+java --add-opens java.base/java.lang=ALL-UNNAMED -jar vape-loader.jar
